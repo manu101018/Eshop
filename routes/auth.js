251c1362284bb70b62625ca043afb0c6848c2991
Manjeet Singh <manjeetsinghmzn2002@gmail.com>
2022-03-23 16:00:11 +0530

    initial commit

---
+++ routes/auth.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const { check,body } = require("express-validator/check");
+
+const authController = require("../controllers/auth");
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get( "/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+  "/login",
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
+    body('password','Password has to be valid.')
+      .isLength({min:5})
+      .isAlphanumeric()
+      .trim()
+  ],
+  authController.postLogin);
+
+router.post(
+  "/signup",
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value,{req})=>{
+        return User.findOne({email:value}).then(userDoc =>{
+          if (userDoc) {
+            return Promise.reject('E-mail exists already, please pick a different one.')
+          };
+        });
+      })
+      .normalizeEmail(),
+    body('password','Please a password with only numbers and text at lest 5 character long')
+      .isLength({min: 5})
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+    .trim()
+      .custom((value, {req})=>{
+        if(value !== req.body.password){
+          throw new Error('Password have to match!');
+        }
+        return true;
+      })
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;
